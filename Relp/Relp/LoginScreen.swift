7768a3c396db7fda8c2c78a7ff7c92d3ddce30fa
Kyle Politz <kpolitz2016@room-9.local>
2016-05-11 13:14:30 -0700

    Added buttons on yellow page

--- Relp/Relp/LoginScreen.swift
+++ Relp/Relp/LoginScreen.swift
@@ -12,7 +12,6 @@ import Foundation
 
 class LoginScreenViewController: UIViewController {
     
-
     @IBOutlet weak var usernameText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     
