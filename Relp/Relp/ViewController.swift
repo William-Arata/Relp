2a3a7a81638cdb4b9883477d985bce29a3fb5e84
Haoyuan (James) Huang <hhuang2017@room208-3.local>
2016-05-17 13:33:25 -0700

    Jame is stupid

--- Relp/Relp/ViewController.swift
+++ Relp/Relp/ViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 import MessageUI
 import Foundation
 
+var controller = ViewController()
+
+struct Data {
+    var click1 = controller.imageButton1
+    var click2 = controller.imageButton2
+    var click3 = controller.imageButton3
+    var description1: String = "\(controller.descriptionLabel1)"
+    var description2: String = "\(controller.descriptionLabel2)"
+    var description3: String = "\(controller.descriptionLabel3)"
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var descriptionLabel1: UILabel!
+    @IBOutlet weak var descriptionLabel2: UILabel!
+    @IBOutlet weak var descriptionLabel3: UILabel!
     
         override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,4 +39,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let data = Data()
+        if let destinationViewController = segue.destinationViewController as? CommentScreenViewController {
+            destinationViewController.data = data
+        }
+    }
+    @IBAction func imageButton1(sender: AnyObject) {
+        let click = 1
+        
+    }
+    @IBAction func imageButton2(sender: AnyObject) {
+        let click = 2
+        
+    }
+    @IBAction func imageButton3(sender: AnyObject) {
+        let click = 3
+        
+    }
+    }
